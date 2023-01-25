a32acca58cae7503a728090da5ee4565b38da742
Alan Nogueira <alan.nogueira991@gmail.com>
2023-01-25 07:56:33 -0300

    concluído projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("88 9 8153-9093", "Modelo1", "IMEI1", 256);
+Nokia nokia = new Nokia("88 9 8153-9093", "Modelo2", "IMEI2", 256);
+
+iphone.InstalarAplicativo("whatsapp");
+nokia.InstalarAplicativo("whatsapp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+System.Console.WriteLine(iphone);
+System.Console.WriteLine(nokia);
